325c82c6bc6d2d6e0a081f5c6d30999c664522b8
trifonova_o_e <myowl@list.ru>
2025-01-13 15:49:11 +0300

    small change in main

--- src/main/java/ru/trifonova/todolist/TodolistApplication.java
+++ src/main/java/ru/trifonova/todolist/TodolistApplication.java
@@ -2,8 +2,11 @@ package ru.trifonova.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.trifonova.todolist.config.DatabaseConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DatabaseConfig.class)
 public class TodolistApplication {
 
 	public static void main(String[] args) {
